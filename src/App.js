e26a9fc6be456507b22333d0133ba45ddb1bc3c7
ddanilets <ddanilets@gmail.com>
2018-06-09 19:02:26 +0300

    blab

--- src/App.js
+++ src/App.js
@@ -55,6 +55,22 @@ class App extends Component {
                 population: 1000,
                 routes: this.state.routes,
             },
+        }, (_, _2, body) => {
+            Object.defineProperty(Array.prototype, 'chunk_inefficient', {
+                value: function(chunkSize) {
+                    var array=this;
+                    return [].concat.apply([],
+                      array.map(function(elem,i) {
+                          return i%chunkSize ? [] : [array.slice(i,i+chunkSize)];
+                      })
+                    );
+                }
+            });
+            const temp = body.chunk_inefficient(10).reverse();
+
+            this.setState({ response: temp.reduce((a, b) => {
+                return a.concat(b);
+            }, []) });
         });
     }
 
@@ -63,7 +79,16 @@ class App extends Component {
 
         for (let i = 0; i < 100; i++) {
             let idName = 'kvartal_' + i;
-            blocks.push(<div className='kvartals' id={idName} key={i}></div>);
+            blocks.push(
+              <div style={{
+              backgroundColor: this.state.response ? this.state.response[i].color === 1 ? 'green' : this.state.response[i].color === 2 ? 'yellow' : 'red' : '',
+              borderBottom: this.state.response ? this.state.response[i].sides.bottom === 1 ? '1px solid green' : this.state.response[i].sides.bottom === 2 ? '1px solid yellow' : '1px solid red' : '',
+              borderTop: this.state.response ? this.state.response[i].sides.top === 1 ? '1px solid green' : this.state.response[i].sides.top === 2 ? '1px solid yellow' : '1px solid red' : '',
+              borderLeft: this.state.response ? this.state.response[i].sides.left === 1 ? '1px solid green' : this.state.response[i].sides.left === 2 ? '1px solid yellow' : '1px solid red' : '',
+              borderRight: this.state.response ? this.state.response[i].sides.right === 1 ? '1px solid green' : this.state.response[i].sides.right === 2 ? '1px solid yellow' : '1px solid red' : '',
+              }} className='kvartals' id={idName} key={i}>
+
+              </div>);
         }
 
         return <div>
